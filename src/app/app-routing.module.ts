796e7362b6c30231fbf5fa98c241301b96b117fd
Rodolfo Obreque <rodolfoobreque19@gmail.com>
2025-05-30 18:50:54 -0400

    Se modifico app.routes.ts y se agrego el archivo app-routing.module,ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,22 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { CourseListComponent } from './components/course-list/course-list.component';
 import { EvaluationFormComponent } from './components/evaluation-form/evaluation-form.component';
 import { TeacherListComponent } from './components/teacher-list/teacher-list.component';
 import { HistoricalEvaluationsComponent } from './components/historical-evaluations/historical-evaluations.component';
 import { ChartsComponent } from './components/charts/charts.component';
-// Importa otros componentes necesarios para rutas
 
-export const routes: Routes = [
-  { path: '', component: CourseListComponent },
+const routes: Routes = [
+  { path: '', redirectTo: '/courses', pathMatch: 'full' },
   { path: 'courses', component: CourseListComponent },
   { path: 'evaluate/:id', component: EvaluationFormComponent },
   { path: 'teachers', component: TeacherListComponent },
   { path: 'historical', component: HistoricalEvaluationsComponent },
-  { path: 'charts', component: ChartsComponent },
-  // ... otras rutas
+  { path: 'charts', component: ChartsComponent }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
